ce7bfc0f07292c4898fd5ac63fe573af6cb87241
Joaquin Bassano <joaquinbas98@gmail.com>
2023-03-07 21:39:59 -0300

    feat: add linter

--- projects/01-hola-mundo/src/App.jsx
+++ projects/01-hola-mundo/src/App.jsx
@@ -3,16 +3,16 @@ import './App.css'
 const App = () => {
   return (
     <div>
-      <h1 style={{ color: '#fff'}}>Twitter Card</h1>
+      <h1 style={{ color: '#fff' }}>Twitter Card</h1>
       <article className='tw-followCard'>
         <header className='tw-followCard-header'>
-          <img className='tw-followCard-avatar' src="https://ui-avatars.com/api/?name=Joa+Bas" alt="El avatar de Joa" />
+          <img className='tw-followCard-avatar' src='https://ui-avatars.com/api/?name=Joa+Bas' alt='El avatar de Joa' />
           <div className='tw-followCard-info'>
             <strong>Joaquin Bassano</strong>
             <span className='tw-followCard-infoUserName'>@joabas98</span>
           </div>
         </header>
-      
+
         <aside>
           <button className='tw-followCard-button'>Seguir</button>
         </aside>
